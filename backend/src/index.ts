d74b8525c0d3639df1305008590732b41ee29ed6
suprithsk <suprithsk2001@gmail.com>
2025-05-04 10:55:28 +0530

    added shadcn

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import authRouter from './routes/authRoutes';
-import blogRouter from './routes/blogRoutes';
+
 dotenv.config();
 
 const app = express();
@@ -11,8 +10,6 @@ app.use(express.json());
 app.use(cors());
 const PORT=5000;
 
-app.use('/auth', authRouter);
-app.use('/blog', blogRouter);
 
 app.listen(PORT, ()=>{
     console.log('app running on:', PORT)
